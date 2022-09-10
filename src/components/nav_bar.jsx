25a0a96354d81b6f0ca636218ab178aa4be0c1c6
kokosevi <s.sager91@gmail.com>
2022-09-10 17:33:03 +0200

    update

---
+++ src/components/nav_bar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class nav_bar extends Component {
+    state = {  } 
+    render() { 
+        
+        return (
+        <div>
+            <h1>Is this working?</h1>
+            <button>Next?</button>
+        </div>        
+        );
+            
+                
+            
+
+        
+    }
+}
+ 
+export default nav_bar;
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import nav_bar from './components/nav_bar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <nav_bar />
   </React.StrictMode>
 );
 
